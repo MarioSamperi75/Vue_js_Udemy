218defce685c175b6a09076ee16af2b1e8d1c2f5
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-02-02 22:09:08 +0100

    vue simple app

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,18 +1,29 @@
-// Javascript traditional approach
-// get the elements
-// create a listener
-// create a function that the listener invokes
+// Vue approach:
 
-const buttonEl = document.querySelector("button");
-const inputEl = document.querySelector("input");
-const listEl = document.querySelector("ul");
+// const buttonEl = document.querySelector("button");
+// const inputEl = document.querySelector("input");
+// const listEl = document.querySelector("ul");
 
-buttonEl.addEventListener("click", addGoal);
+// buttonEl.addEventListener("click", addGoal);
 
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement("li");
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = "";
-}
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement("li");
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = "";
+//}
+
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+    },
+  },
+}).mount("#app");
